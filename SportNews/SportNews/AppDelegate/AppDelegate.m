f30667b3e7e7adb2b738333d2e5dd48d6269be11
frank1liu <frankliu525@gmail.com>
2025-03-13 20:11:21 +0800

    [Modify][Update]-移除open install lib

--- SportNews/SportNews/AppDelegate/AppDelegate.m
+++ SportNews/SportNews/AppDelegate/AppDelegate.m
@@ -31,9 +31,9 @@
 #import <AlicloudCrash/AlicloudCrashProvider.h>
 #import <AlicloudHAUtil/AlicloudHAProvider.h>
 
-#import "OpenInstallSDK.h"
+// #import "OpenInstallSDK.h"
 
-@interface AppDelegate ()<JPUSHRegisterDelegate, BuglyDelegate, OpenInstallDelegate>
+@interface AppDelegate ()<JPUSHRegisterDelegate, BuglyDelegate/*, OpenInstallDelegate*/>
 
 @property(nonatomic, assign) BOOL isSuccess;
 @property (nonatomic, strong) NSURLSession *session;
